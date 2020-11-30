4c744f7c7f98d044a4b72cb128ae2e45aa6f7037
Anthony Nguyen <anthonynguyen@Anthonys-MacBook-Pro.local>
2020-11-30 14:53:21 +1100

    add jwt generator

--- LegacyMVC/LegacyMvc/Controllers/HomeController.cs
+++ LegacyMVC/LegacyMvc/Controllers/HomeController.cs
@@ -7,6 +7,19 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
 
 namespace ETaxPoc.Controllers
 {
@@ -35,6 +48,20 @@ namespace ETaxPoc.Controllers
             return View();
         }
 
+        // Return JWT token which can be used to authenticate this user in the new backend
+        [Authorize]
+        public string Token()
+        {
+            var isAuthenticated = HttpContext.User.Identity.IsAuthenticated;
+            if (!isAuthenticated)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            return generateJwtToken(new User () {
+                Username = HttpContext.User.Identity.Name 
+            });
+        }
+
         [Authorize]
         public async Task<IActionResult> Logout()
         {
@@ -51,7 +78,7 @@ namespace ETaxPoc.Controllers
                 {
                     Username = "David John"
                 } : null;
-                
+
                 if (user == null)
                 {
                     TempDataMessage("message", "danger", $"Incorrect Password or Email.");
@@ -89,7 +116,23 @@ namespace ETaxPoc.Controllers
             {
                 Debug.WriteLine("TempDataMessage Error");
             }
+        }
 
+        private string generateJwtToken(User user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("this is a very long secret which need to be the same in both backend");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                    new Claim("id", user.Username.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
